4e46e59546e1a178386cddee6c385537d5a01770
christopher_pc <christopherjmacgregor@gmail.com>
2019-11-20 13:38:07 -0600

    hw11

--- hw11/DoublyLinkedList.h
+++ hw11/DoublyLinkedList.h
@@ -6,6 +6,8 @@
 #include <iomanip>
 #include <sstream>
 
+using namespace std;
+
 //////////////////////////////////////////////////////////////////////////////
 //                         class template definition                        //
 //////////////////////////////////////////////////////////////////////////////
@@ -26,14 +28,14 @@ public:
     const T& back() const { return tail->data; };
     void push_back(T);
     void push_front(T);
-    // void pop_back();
-    // void pop_front();
-    // void erase(unsigned int);
-    // void insert(T data, unsigned int idx);
+    void pop_back();
+    void pop_front();
+    void erase(unsigned int);
+    void insert(T data, unsigned int index);
     void clear();
     std::string to_str() const;
     // breaks encapsulation... gives operator== access to private data... not great approach, but makes the homework easier for you since we haven't talked about iterators :).
-    // template<typename U> friend bool operator==(DoublyLinkedList<U> const&, DoublyLinkedList<U> const&);
+    template<typename U> friend bool operator==(DoublyLinkedList<U> const&, DoublyLinkedList<U> const&);
 private:
     // dO nOt CHanGe oRdEriNg.
     Node<T> *head;
@@ -68,7 +70,10 @@ DoublyLinkedList<T>::DoublyLinkedList(T data) {
 }
 
 template<typename T>
-DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList<T>& tocopy): head(nullptr), tail(nullptr), sz(0) {
+DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList<T>& tocopy) {
+  this->head = nullptr;
+  this->tail = nullptr;
+  this->sz = 0;
   Node<T>* current = tocopy.head;
   while (current) {
     this->push_back(current->data);
@@ -78,10 +83,22 @@ DoublyLinkedList<T>::DoublyLinkedList(const DoublyLinkedList<T>& tocopy): head(n
 
 template<typename T>
 DoublyLinkedList<T>& DoublyLinkedList<T>::operator=(const DoublyLinkedList<T>& tocopy) {
-  Node<T>* current = tocopy.head;
-  while (current) {
-    this->push_back(current->data);
-    current = current->next;
+  if (this != &tocopy) {
+    Node<T>* clear = this->head;
+     while (clear) {
+      Node<T>* next = clear->next;
+      delete clear;
+      clear = next;
+    }
+    sz = 0;
+    head = nullptr;
+    tail = nullptr;
+
+    Node<T>* current = tocopy.head;
+    while (current) {
+      this->push_back(current->data);
+      current = current->next;
+    }
   }
   return* this;
 }
@@ -119,6 +136,96 @@ void DoublyLinkedList<T>::push_front(T elem) {
     head->prev = node;
     node->next = head;
     head = node;
+    sz++;
+  }
+}
+
+template<typename T>
+void DoublyLinkedList<T>::pop_back() {
+  if (!head) {
+    return;
+  }
+  if (sz > 1) {
+    Node<T>* secondlast = this->tail->prev;
+    secondlast->next = nullptr;
+    delete tail;
+    tail = secondlast;
+    tail->next = nullptr;
+    sz--;
+  } else {
+    delete tail;
+    head = nullptr;
+    tail = head;
+    sz--;
+  }
+}
+
+template<typename T>
+void DoublyLinkedList<T>::pop_front() {
+  if (!head) {
+    return;
+  }
+  if (sz > 1) {
+    Node<T>* secondfront = this->head->next;
+    delete head;
+    head = secondfront;
+    secondfront->prev = nullptr;
+    sz--;
+  } else {
+    delete head;
+    head = nullptr;
+    tail = head;
+    sz--;
+  }
+}
+
+template<typename T>
+void DoublyLinkedList<T>::erase(unsigned int index) {
+  Node<T>* targetnode = this->head;
+  if (index > this->sz - 1) {
+    cout << "Invalid index" << endl;
+    return;
+  }
+  if (index == 0) { // erase head
+    pop_front();
+  } else if (index == (this->sz - 1)) { // erase tail
+    pop_back();
+  } else { // erase from middle
+    // for loop gets target node
+    for (unsigned int i = 0; i <= index - 1; i++) {
+      targetnode = targetnode->next;
+    }
+    targetnode->prev->next = targetnode->next;
+    targetnode->next->prev = targetnode->prev;
+    delete targetnode;
+    sz--;
+  }
+}
+
+template<typename T>
+void DoublyLinkedList<T>::insert(T data, unsigned int index) {
+  Node<T>* atposition = this->head;
+  Node<T>* atpositionPrev;
+  if (index > this->sz) {
+    cout << "Invalid index" << endl;
+    return;
+  }
+  Node<T>* toinsert = new Node<T>(data);
+  if (index == 0) { // head
+    push_front(data);
+  } else if (index == (this->sz)) { // tail
+    push_back(data);
+  } else { // input in middle
+    for (unsigned int i = 0; i <= index - 1; i++) { // set the position
+      atposition = atposition->next;
+    }
+    cout << atposition << endl; //testline**********
+    atpositionPrev = atposition->prev;
+    toinsert->next = atposition;
+    toinsert->prev = atpositionPrev;
+
+    atposition->prev = toinsert;
+    atpositionPrev->next = toinsert;
     sz++;
   }
 }
@@ -164,7 +271,19 @@ std::string DoublyLinkedList<T>::to_str() const
 template<typename T>
 bool operator==(DoublyLinkedList<T> const& lhs, DoublyLinkedList<T> const& rhs)
 {
-    // getting you started on this once... definition for this overloaded operator will go here.
+    Node<T>* right = rhs.head;
+    Node<T>* left = lhs.head;
+    if (rhs.sz != lhs.sz) {
+      return false;
+    }
+    while (right) {
+      if (right->data == left->data) {
+        right = right->next;
+        left = left->next;
+      } else {
+        return false;
+      }
+    }
     return true;
 }
 
--- hw11/driver.cpp
+++ hw11/driver.cpp
@@ -6,7 +6,14 @@ using namespace std;
 
 int main()
 {
+  DoublyLinkedList<int> list;
+  // DoublyLinkedList<int> list2 = list;
+  // list2.pop_back();
+  DoublyLinkedList<int> list2;
+  list2 = list;
 
-
+  cout << list << endl;
+  cout << list2 << endl;
+  // cout << list3 << endl;
     return 0;
 }
