ad2d0d98ba7c8696b78541d591db948f4b2eff47
Christopher McGregor <christopherjmacgregor@gmail.com>
2021-10-16 18:41:15 -0500

    pa3 skeleton code

---
+++ CSCE313-1/PA3/client.cpp
@@ -0,0 +1,90 @@
+#include "FIFOreqchannel.h"
+#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "common.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	int opt;
+	int person_num = 1;
+	double time = 0.0;
+	int ecg_num = 1;
+	string filename = "";
+	// take all the arguments first because some of these may go to the server
+	while ((opt = getopt(argc, argv, "p:t:e:f:")) != -1) {
+		switch (opt) {
+			case 'f':
+				filename = optarg;
+				break;
+			case 'p':
+				person_num = stoi(optarg);
+				break;
+			case 't':
+				time = stod(optarg);
+				break;
+			case 'e':
+				ecg_num = stoi(optarg);
+				break;
+			case '?':
+        if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+		}
+	}
+
+	int pid = fork ();
+	if (pid < 0){
+		EXITONERROR ("Could not create a child process for running the server");
+	}
+	if (!pid){ // The server runs in the child process
+		char* args[] = {"./server", nullptr};
+		if (execvp(args[0], args) < 0){
+			EXITONERROR ("Could not launch the server");
+		}
+	}
+	FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
+	cout << "here" << endl;
+	DataRequest d (person_num, time, ecg_num);
+	chan.cwrite (&d, sizeof (DataRequest)); // question
+	double reply;
+	chan.cread (&reply, sizeof(double)); //answer
+	if (isValidResponse(&reply)){
+		cout << "For person " << person_num <<", at time " << time << ", the value of ecg "<< ecg_num <<" is " << reply << endl;
+	}
+
+
+	/* this section shows how to get the length of a file
+	you have to obtain the entire file over multiple requests
+	(i.e., due to buffer space limitation) and assemble it
+	such that it is identical to the original*/
+	FileRequest fm (0,0);
+	// cout << "filename:" << filename << endl;
+	int len = sizeof (FileRequest) + filename.size()+1;
+	char buf2 [len];
+	memcpy (buf2, &fm, sizeof (FileRequest));
+	strcpy (buf2 + sizeof (FileRequest), filename.c_str());
+	chan.cwrite (buf2, len);
+	int64 filelen;
+	chan.cread (&filelen, sizeof(int64));
+	if (isValidResponse(&filelen)){
+		cout << "File length is: " << filelen << " bytes" << endl;
+	}
+
+
+	// closing the channel
+    Request q (QUIT_REQ_TYPE);
+    chan.cwrite (&q, sizeof (Request));
+	// client waiting for the server process, which is the child, to terminate
+	wait(0);
+	cout << "Client process exited" << endl;
+
+}
