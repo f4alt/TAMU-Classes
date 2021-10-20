16068d7b817f98751fcae30d81167cb50ec69fd5
f4alt <christopherjmacgregor@gmail.com>
2021-10-20 18:36:12 -0500

    support buffer size up to 1Mb

--- CSCE313-1/PA3/client.cpp
+++ CSCE313-1/PA3/client.cpp
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]){
 		EXITONERROR ("Could not create a child process for running the server");
 	}
 	if (!pid){ // The server runs in the child process
-		char buffer_size_string[4 + sizeof(char)];
+		char buffer_size_string[7 + sizeof(char)];
 		sprintf(buffer_size_string, "%d", buffer_size);
 		char* args[] = {"./server", "-m", buffer_size_string, nullptr};
 		if (execvp(args[0], args) < 0){
