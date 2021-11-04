33769f0ed4c59de460361514feea872368b750fa
f4alt <christopherjmacgregor@gmail.com>
2021-11-04 05:17:41 -0500

    code test

--- CSCE313-1/PA4/client.cpp
+++ CSCE313-1/PA4/client.cpp
@@ -38,8 +38,8 @@ void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, Hi
 	double resp = 0;
 	while (1) {
 		req_buf->pop(buf, 1024);
-		REQUEST_TYPE_PREFIX m = ((Request*)buf)->getType();
 		cout << "m:" << m << endl;
+		REQUEST_TYPE_PREFIX m = ((Request*)buf)->getType();
 
 		if (m == DATA_REQ_TYPE) {
 			chan->cwrite(&buf, sizeof(DataRequest));
