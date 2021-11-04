6e25e2801040b0b259a2445aef8692bd8556eb78
f4alt <christopherjmacgregor@gmail.com>
2021-11-04 05:54:27 -0500

    code test

--- CSCE313-1/PA4/client.cpp
+++ CSCE313-1/PA4/client.cpp
@@ -27,7 +27,7 @@ void patient_thread_function(int n, int pat_num, BoundedBuffer* req_buf) {
 		// chan->cwrite(&d, sizeof(DataRequest));
 		// chan->cread(&resp, sizeof(double));
 		// hc->update(pat_num, resp);
-		// req_buf->push((char *)&d, sizeof(DataRequest));
+		req_buf->push((char *)&d, sizeof(DataRequest));
 		d.seconds += 0.004;
 	}
 }
@@ -47,8 +47,9 @@ void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, Hi
 		// } else if (m == FILE_REQ_TYPE) {
 		//
 		// } else if (m == QUIT_REQ_TYPE) {
-		// 	chan->cwrite(&m, sizeof(Request));
-		// 	delete chan;
+			Request m (QUIT_REQ_TYPE);
+			chan->cwrite(&m, sizeof(Request));
+			delete chan;
 		// 	break;
 		// }
 	// }
