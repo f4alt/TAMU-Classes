65b2b4df0a88335cd62ce4fce2293354ec4050f0
f4alt <christopherjmacgregor@gmail.com>
2021-11-08 14:19:56 -0600

    code test

--- CSCE313-1/PA4/client.cpp
+++ CSCE313-1/PA4/client.cpp
@@ -46,7 +46,7 @@ void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, Bo
 			// cout << "data req" << endl;
 			DataRequest* dm = (DataRequest*)r;
 			chan->cwrite(&dm, sizeof(DataRequest));
-			chan->cread(&resp, sizeof(double));
+			// chan->cread(&resp, sizeof(double));
 			// hc->update(((DataRequest*)r)->person, resp);
 
 			// hist_buf->push((char*)&dm, sizeof(DataRequest));
