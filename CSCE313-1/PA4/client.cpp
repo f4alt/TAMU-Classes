8d1dae0b0d02b0f3e48160f147c228b0ee207439
f4alt <christopherjmacgregor@gmail.com>
2021-11-08 22:05:09 -0600

    basic functionality works

--- CSCE313-1/PA4/client.cpp
+++ CSCE313-1/PA4/client.cpp
@@ -6,10 +6,10 @@
 #include <thread>
 using namespace std;
 
-FIFORequestChannel* create_channel(FIFORequestChannel* chan, int buffer_size) {
+FIFORequestChannel* create_channel(FIFORequestChannel* chan) {
 	Request nc (NEWCHAN_REQ_TYPE);
 	chan->cwrite(&nc, sizeof(Request));
-	char buf3[buffer_size];
+	char buf3[1024];
 	chan->cread(buf3, sizeof(buf3));
 	string new_chan_name = buf3;
 
@@ -17,69 +17,35 @@ FIFORequestChannel* create_channel(FIFORequestChannel* chan, int buffer_size) {
 	FIFORequestChannel* new_chan = new FIFORequestChannel(new_chan_name, FIFORequestChannel::CLIENT_SIDE);
 
 	return new_chan;
-
-	// return FIFORequestChannel(new_chan_name, FIFORequestChannel::CLIENT_SIDE);
 }
 
-void patient_thread_function(int n, int pat_num, BoundedBuffer* req_buf, FIFORequestChannel* chan) {
-	DataRequest d(pat_num, 0.0, 1);
-	double resp = 0;
-	cout << "n: " << n << endl;
-
-	for (int i=0; i < n; i++) {
-		// chan->cwrite(&d, sizeof(DataRequest));
-		// chan->cread(&resp, sizeof(double));
-		// hc->update(pat_num, resp);
-
-
+void patient_thread_function(int n, int pno, BoundedBuffer* request_buffer) {
+	DataRequest d(pno, 0.0, 1);
+	for (int i =0; i < n; i++) {
 		vector<char> v = vector<char>((char*)&d, (char*)&d + sizeof(DataRequest));
-		req_buf->push(v);
-
-		vector<char> req_check = req_buf->pop();
-		Request* r = (Request*)req_check.data();
-		DataRequest* dm = (DataRequest*)r;
-		chan->cwrite(&dm, sizeof(DataRequest));
-		// cout << "why are we reading here" << endl;
-		chan->cread(&resp, sizeof(double));
-		cout << "resp check | patient:" << dm->person << " seconds:" << dm->seconds << " resp:" << resp << endl;
-		// cout << "req check for person:" << ((DataRequest*)dm)->person << " | i:" << i << endl;
-
-		// req_buf->push((char*)&d, sizeof(DataRequest));
-		d.seconds += 0.004;
+		request_buffer->push(v);
+		d.seconds += .004;
 	}
 }
 
 
 // void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, BoundedBuffer* hist_buf, HistogramCollection* hc){
-void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, HistogramCollection* hc){
+void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* request_buffer, HistogramCollection* hc){
+	vector<char> msg;
 	double resp = 0;
-	Request q(QUIT_REQ_TYPE);
-	while (1) {
-		vector<char> req = req_buf->pop();
-		// char* data = req.data();
-		// Request* r = (Request*)data;
-		Request* r = (Request*)req.data();
+	while(1) {
+		msg = request_buffer->pop();
+		Request* r = (Request*)msg.data();
 
 		if (r->getType() == DATA_REQ_TYPE) {
 			// cout << "data req" << endl;
-			// DataRequest* dm = (DataRequest*)r;
-			// chan->cwrite(&dm, sizeof(DataRequest));
-			// // cout << "why are we reading here" << endl;
-			// chan->cread(&resp, sizeof(double));
-			// cout << "updating person:" << ((DataRequest*)dm)->person << endl;
-			// hc->update(((DataRequest*)dm)->person, resp);
+			chan->cwrite((char*)r, sizeof(DataRequest));
+			chan->cread(&resp, sizeof(double));
+			hc->update(((DataRequest*)r)->person, resp);
+		} else if (r->getType() == FILE_REQ_TYPE) {
 
-			// hist_buf->push((char*)&dm, sizeof(DataRequest));
-		}  if (r->getType() == FILE_REQ_TYPE) {
-			cout << "file req" << endl;
-			// int flen = sizeof(FileRequest) + sizeof(filename) + 1;
-			// char buf[flen];
-			// vector<char> v = vector<char>((char*)&buf, (char*)&buf + flen);
-			// req_buf->push(v);
-		}  if (r->getType() == QUIT_REQ_TYPE) {
-			cout << "quit req" << endl;
-			// Request q (QUIT_REQ_TYPE);
-			chan->cwrite(&q, sizeof(Request));
+		} else if (r->getType() == QUIT_REQ_TYPE) {
+			chan->cwrite(r, sizeof(Request));
 			delete chan;
 			break;
 		}
@@ -88,19 +54,7 @@ void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, Hi
 
 
 void histogram_thread_function (FIFORequestChannel* chan, BoundedBuffer* hist_buf, HistogramCollection* hc){
-	double resp = 0;
-	// while (1) {
-	// 	vector<char> req = hist_buf->pop();
-	// 	char* m = (char*)req.data();
-	//
-	// 	if (*(REQUEST_TYPE_PREFIX*)m == DATA_REQ_TYPE) {
-	// 		DataRequest* dm = (DataRequest*)m;
-	// 		// hist_buf->push((char*)&dm, sizeof(DataRequest));
-	// 		chan->cwrite(&dm, sizeof(DataRequest));
-	// 		chan->cread(&resp, sizeof(double));
-	// 		hc->update(((DataRequest*)dm)->person, resp);
-	// 	}
-	// }
+
 }
 
 
@@ -189,84 +143,47 @@ int main(int argc, char *argv[]){
 	}
 	FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
 	BoundedBuffer request_buffer(b);
-	BoundedBuffer histogram_buffer(b);
 	HistogramCollection hc;
 
-	// make p histograms and add to collection - ie 1 per person
-	for (int i =0; i < p; i++) {
+	// create one histogram per patient and add to collection
+	for (int i = 0; i < p; i++) {
 		Histogram* h = new Histogram(10, -2.0, 2.0);
 		hc.add(h);
 	}
-	cout << p << " histograms in collection" << endl;
 
-	// create w worker channels
-	FIFORequestChannel* wchans[p];
-	for (int i =0; i < w; i++) {
-		// wchans[i] = create_channel(&chan, m);
-
-		Request nc (NEWCHAN_REQ_TYPE);
-		chan.cwrite(&nc, sizeof(Request));
-		char buf3[m];
-		chan.cread(buf3, sizeof(buf3));
-		string new_chan_name = buf3;
-
-		// cout << "new channel created, name: " << new_chan_name << endl;
-		wchans[i] = new FIFORequestChannel(new_chan_name, FIFORequestChannel::CLIENT_SIDE);
+	// make worker channels
+	FIFORequestChannel* wchans[w];
+	for (int i = 0; i < w; i++) {
+		wchans[i] = create_channel(&chan);
 	}
-	cout << "created " << w << " worker channels" << endl;
-
-
-
 
 	struct timeval start, end;
     gettimeofday (&start, 0);
 
     /* Start all threads here */
-
-		// start p patient threads
 		thread patient[p];
-		for (int i =0; i < p; i++) {
-			// void patient_thread_function(int n, int pat_num, BoundedBuffer* req_buf, FIFORequestChannel* chan) {
-			patient[i] = thread(patient_thread_function, n, i+1, &request_buffer, &chan);
+		for (int i=0; i < p; i++) {
+			patient[i] = thread(patient_thread_function, n, i+1, &request_buffer);
 		}
-		cout << "started " << p << " patient thread(s)" << endl;
-
 		thread workers[w];
 		for (int i = 0; i < w; i++) {
-			// workers[i] = thread(worker_thread_function, wchans[i], &request_buffer, &histogram_buffer, &hc);
-			//void worker_thread_function(FIFORequestChannel* chan, BoundedBuffer* req_buf, HistogramCollection* hc){
-				workers[i] = thread(worker_thread_function, wchans[i], &request_buffer, &hc);
+			workers[i] = thread(worker_thread_function, wchans[i], &request_buffer, &hc);
 		}
-		cout << "started " << w << " worker thread(s)" << endl;
-
-		// thread hists[h];
-		for (int i =0; i < h; i++) {
-			// hists[i] = thread(histogram_thread_function, &chan, &histogram_buffer, &hc);
-		}
-		cout << "started " << h << " hists thread(s)" << endl;
-
 
 		/* Join all threads here */
-		for (int i =0; i < p; i++) {
+		for (int i = 0; i < p; i++) {
 			patient[i].join();
 		}
-		cout << "patients joined" << endl;
-		for (int i = 0; i < w; i++) {
-			Request q (QUIT_REQ_TYPE);
 
+		for (int i =0; i<w; i++) {
+			Request q (QUIT_REQ_TYPE);
 			vector<char> v = vector<char>((char*)&q, (char*)&q + sizeof(Request));
 			request_buffer.push(v);
-
-			// request_buffer.push((char*)&q, sizeof(Request));
 		}
-		for (int i =0; i < w; i++) {
+		for (int i=0; i<w; i++) {
 			workers[i].join();
 		}
-		cout << "workers joined" << endl;
-		for(int i=0; i < h; i++) {
-			// hists[i].join();
-		}
-		cout << "hists joined" << endl;
+
     gettimeofday (&end, 0);
 
     // print the results and time difference
@@ -276,14 +193,11 @@ int main(int argc, char *argv[]){
     cout << "Took " << secs << " seconds and " << usecs << " micro seconds" << endl;
 
 
-		// CLEAN UP ALL CHANNELS
-    Request q (QUIT_REQ_TYPE);
-		// clean up worker channels
-		// for (int i =0; i < p; i++) {
-		// 	wchans[i]->cwrite(&q, sizeof(Request));
-		// 	delete wchans[i];
-		// }
-		// clean up control
+		// CLEAN UP CHANNELS
+		Request q (QUIT_REQ_TYPE);
+		for (int i = 0; i < p; i++) {
+			wchans[i]->cwrite(&q, sizeof(Request));
+		}
     chan.cwrite (&q, sizeof (Request));
 
 	// client waiting for the server process, which is the child, to terminate
